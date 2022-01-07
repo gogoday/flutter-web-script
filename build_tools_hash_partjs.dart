ab7be995bba9f46ef4db84fff586219abcf2db77
sampsonwang <sampsonwang@tencent.com>
2022-01-07 19:31:03 +0800

    feat: no part file

--- build_tools_hash_partjs.dart
+++ build_tools_hash_partjs.dart
@@ -1,14 +1,13 @@
 import 'dart:io';
-import 'dart:convert';
 import 'package:crypto/crypto.dart';
 
 const basePath = 'build/web';
 void main() async {
-  File file = new File('${basePath}/main.dart.js');
+  File file = File('$basePath/main.dart.js');
   try{
     String content = await file.readAsString();
     // print(content);
-    RegExp exp = new RegExp(r"deferredPartUris:\[.*\]");
+    RegExp exp = RegExp(r"deferredPartUris:\[.*\]");
     Iterable<Match> matchs = exp.allMatches(content);
 
     for (Match m in matchs) {
@@ -16,20 +15,24 @@ void main() async {
       String? match2 = '';
 
       if (match is String) {
-        RegExp expJsFile = new RegExp(r"main\.dart\.js_\d+\.part\.js");
+        RegExp expJsFile = RegExp(r"main\.dart\.js_\d+\.part\.js");
         Iterable<Match> filesName = expJsFile.allMatches(match);
         for (Match f in filesName) {
           String? jsPartName = f.group(0);
           if (jsPartName is String) {
-            var file = File('${basePath}/${jsPartName}');
+            var file = File('$basePath/$jsPartName');
             String md5Str = md5.convert(file.readAsBytesSync()).toString().substring(0, 10);
-            String newName = jsPartName.replaceFirst('part', 'part-${md5Str}');
-            file.renameSync('${basePath}/${newName}');
-            match2 = match.replaceFirst(jsPartName, newName) as String;
+            String newName = jsPartName.replaceFirst('part', 'part-$md5Str');
+            file.renameSync('$basePath/$newName');
+            match2 = match.replaceFirst(jsPartName, newName);
           }
         }
       }
-      print('s/deferredPartUris:.*/${match2},/g');
+      // no part file
+      if (match2 == '') {
+        exit(0);
+      }
+      print('s/deferredPartUris:.*/$match2,/g');
       exit(0);
     }
   }catch(e){
